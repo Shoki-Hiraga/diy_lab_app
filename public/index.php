fa18b4c950812649422096667d3084cacc81f26e
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-12-12 01:16:58 +0900

    index.phpをローカル用とパブリック用で構成

--- public/index.php
+++ public/index.php
@@ -1,15 +1,43 @@
 <?php
+// ローカル用index.php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-require __DIR__.'/../diy_lab_app/vendor/autoload.php';
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-$app = require_once __DIR__.'/../diy_lab_app/bootstrap/app.php';
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
+
+
+/*  Xサーバー用のindex.php
+<?php
+
+// Xserver のドキュメントルート public_html から
+// Laravel の public ディレクトリへ流す
+
+require __DIR__ . '/diy_lab_app/vendor/autoload.php';
+$app = require_once __DIR__ . '/diy_lab_app/bootstrap/app.php';
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+$kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+    $request = Request::capture()
 )->send();
 
 $kernel->terminate($request, $response);
+*/
