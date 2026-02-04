fd89097669c798a2b975b75fff1b3cf01feb91c5
Shoki-Hiraga <chaser.cb750@gmail.com>
2026-02-05 00:41:57 +0900

    ログイン後のリダイレクト設定変更

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         return redirect()->intended(
-            route('users.posts.index')
+            route('users.top')
         );
 
 
