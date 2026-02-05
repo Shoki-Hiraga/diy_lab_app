daf570d2b51ed83289e9ea1aaff166a034741588
Shoki-Hiraga <chaser.cb750@gmail.com>
2026-02-06 00:23:05 +0900

    ログインCTA設置

---
+++ resources/views/components/common/cta-card.blade.php
@@ -0,0 +1,23 @@
+@guest
+<div class="cta-card">
+    <div class="cta-card__inner">
+        <div class="cta-card__image">
+            <img src="{{ asset('images/cta/mascot.png') }}" alt="">
+        </div>
+
+        <div class="cta-card__body">
+            <h3 class="cta-card__title">
+                知識をシェアしよう！
+            </h3>
+
+            <p class="cta-card__text">
+                あなたの経験や技術を投稿してみませんか？
+            </p>
+
+            <a href="{{ route('register') }}" class="cta-card__button">
+                無料で始める
+            </a>
+        </div>
+    </div>
+</div>
+@endguest
